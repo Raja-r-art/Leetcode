b4e62a32f78131347f8ef850a748cdeb1bae1834
Raja <raja.r2024aiml@sece.ac.in>
2026-09-08 02:54:37 +0530

    Time: 0 ms (100%), Space: 8.9 MB (17.85%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int>num;
+      int fact=1;
+      for(int i=1;i<n;i++){
+        fact=fact*i;
+        num.push_back(i);
+      }
+      k=k-1;
+      string ans="";
+      num.push_back(n);
+      while(true){
+       ans=ans+to_string(num[k/fact]);
+       num.erase(num.begin()+k/fact);
+       if(num.size()==0) break;
+       k=k%fact;
+       fact=fact/num.size();
+      }
+      return ans;
+    }
+};
