bacf36e2d1c775c7a640059dc7c526deff63f2f6
Raja <raja.r2024aiml@sece.ac.in>
2026-09-09 15:39:50 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.39%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,13 @@
 class Solution {
 private:
-int f(int n){
+int f(int n, vector<int>& dp){
     if(n<=1) return n;
-    return f(n-1)+f(n-2);
+    if(dp[n] != -1) return dp[n];
+    return dp[n] = f(n-1, dp)+f(n-2, dp);
 }
 public:
     int fib(int n) {
-        return f(n);
+        vector<int> dp(n + 1, -1);
+        return f(n, dp);
     }
 };
