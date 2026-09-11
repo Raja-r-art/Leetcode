64793b7e28f710fc169cb675849a6d32d740f543
Raja <raja.r2024aiml@sece.ac.in>
2026-09-11 09:42:08 +0530

    Time: 4 ms (1.16%), Space: 16.3 MB (7.34%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,42 +11,39 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (head ==NULL ||head->next==NULL){
-            return head;
-        }
-        ListNode* odd1=head;
-        ListNode* odd2=head->next->next;
-        ListNode* start=head->next;
-        ListNode* end=NULL;
-        ListNode* even1=head->next;
-        ListNode* even2=NULL;
-        if(head->next->next !=NULL) {
-            even2 = head->next->next->next;
-        }
-        while(odd2!=NULL || even2!=NULL){
-            if(even2!=NULL){
-            even1->next=even2;
-            even1=even2;
-            end=even2;
-            if(even2->next!=NULL) {
-                even2=even2->next->next;
-                }else{
-                    even2=NULL;
-                }
-            }
-
-            if(odd2!=NULL){
-            odd1->next=odd2;
-            odd1=odd2;
-            if(odd2->next!=NULL) {
-                odd2=odd2->next->next;
+        if(head==NULL || head->next==NULL) return head;
+        vector<int>evennum;
+        vector<int>oddnum;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        while(odd!=NULL){
+            oddnum.push_back(odd->val);
+            if(odd->next!=NULL){
+                odd =odd->next->next;
             }else{
-                odd2=NULL;
-            }
+                odd=NULL;
             }
         }
-        even1->next=NULL;
-        odd1->next=start;
-        return head;
+        while(even!=NULL){
+            evennum.push_back(even->val);
+            if(even->next!=NULL){
+                even =even->next->next;
+            }else{
+                even=NULL;
+            }
+        }
+        ListNode*temp=new ListNode(0);
+        ListNode*tail=temp;
+        for(int i=0;i<oddnum.size();i++){
+            ListNode* newnode=new ListNode(oddnum[i]);
+            tail->next=newnode;
+            tail=tail->next;
+        }
+        for(int i=0;i<evennum.size();i++){
+            ListNode* newnode=new ListNode(evennum[i]);
+            tail->next=newnode;
+            tail=tail->next;
+        }
+        return temp->next;
     }
 };
