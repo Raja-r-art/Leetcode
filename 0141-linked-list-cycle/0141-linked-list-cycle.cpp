4c4558a866bf1326a5274966376dd7580f8bb2ee
Raja <raja.r2024aiml@sece.ac.in>
2026-09-15 16:32:54 +0530

    Time: 8 ms (80.94%), Space: 11.9 MB (53.33%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,21 +6,17 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- //BRUTE FORCE
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if( head==NULL || head->next==NULL) return false;
-        map<ListNode*,int>mp;
-        ListNode*temp=head;
-        while(temp!=NULL){
-            if(mp.find(temp)!= mp.end()){
-                return true;
-            }else{
-                mp[temp]++;
-            }
-            temp=temp->next;
+        if(head==NULL ) return false;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL &&fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) return true;
         }
-        return false;
+         return false;
     }
 };
