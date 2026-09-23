cdc53292e23f70ec1639fce67b2699955dee8fc5
Raja <raja.r2024aiml@sece.ac.in>
2026-09-24 01:50:11 +0530

    Time: 6 ms (39.89%), Space: 28.7 MB (19.02%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,15 +3,33 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
         vector<int>ans(n,-1);
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<=i+n-1;j++){
-                int idx=j%n;
-                 if(nums[idx]>nums[i]){
-                     ans[i]=nums[idx];
-                     break;
-                 }
+        stack<int>st;
+        for(int i=2*n-1;i>=0;i--){
+            if(st.empty()){
+                st.push(nums[i%n]);
+            }else if(st.top()<=nums[i%n]){
+                while(!st.empty() && st.top()<=nums[i%n]){
+                    st.pop();
+                }
+                st.push(nums[i%n]);
+            }else if(st.top()>nums[i%n]){
+                st.push(nums[i%n]);
+            }
+               if(i<n){
+                if(st.top()<=nums[i]){
+                    while(!st.empty() && st.top()<=nums[i]){
+                        st.pop();
+                    }
+                    if(!st.empty()){
+                        ans[i]=st.top();
+                        st.push(nums[i]);
+                    }
+                    else if(st.empty()){
+                        st.push(nums[i]);
+                    }
+                }
+            }
         }
-    }
-    return ans;
+        return ans;
     }
 };
