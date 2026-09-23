85578463e633cffd1cfb1ed16f2ae0ff393b7969
Raja <raja.r2024aiml@sece.ac.in>
2026-09-23 14:53:26 +0530

    Time: 0 ms (100%), Space: 13 MB (14.61%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,30 +1,31 @@
 class Solution {
-void find(int i,int j,vector<int>& nums1,vector<int>& nums2){
-    int n1=nums1.size();
-    int n2=nums2.size();
-    for(int a=i;a<n1;a++){
-        int val=nums1[a];
-        for(int b=j;b<n2;b++){
-           if(nums2[b]>nums1[a]){
-            nums1[a]=nums2[b];
-            break;
-           }
-        }
-        if(nums1[a]==val) nums1[a]=-1;
-        break;
-    }
-}
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1=nums1.size();
         int n2=nums2.size();
-        for(int i=0;i<n1;i++){
-           for(int j=0;j<n2;j++){
-            if(nums1[i]==nums2[j]){
-                find(i,j,nums1,nums2);
-                break;
+        stack<int>st;
+        map<int,int>mp;
+        for(int i=n2-1;i>=0;i--){
+            if(st.empty()){
+                st.push(nums2[i]);
+                mp[nums2[i]]=-1;
+            }else if(st.top()>nums2[i]){
+                mp[nums2[i]]=st.top();
+                st.push(nums2[i]);
+            }else if(nums2[i]>st.top()){
+                while(!st.empty() && nums2[i]>st.top()){
+                st.pop();
+                }
+                if(st.empty()){
+                    mp[nums2[i]]=-1;
+                }else{
+                     mp[nums2[i]]=st.top();
+                }
+                    st.push(nums2[i]);
             }
-           }
+        }
+        for(int i=0;i<n1;i++){
+            nums1[i]=mp[nums1[i]];
         }
         return nums1;
     }
